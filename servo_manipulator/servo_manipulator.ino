47731cbeafaa0fcc60f9ae5bf57bd253149dba6c
4eburashk <39623246+4eburashk@users.noreply.github.com>
2019-07-26 00:50:54 +0300

    servo

---
+++ servo_manipulator/servo_manipulator.ino
@@ -0,0 +1,65 @@
+// держатель камеры. 2 сервы.
+#include <Servo.h>
+
+Servo SRVup;
+Servo SRVdown;
+
+void setup()
+{
+  SRVup.attach(6);		// 0-89
+  SRVdown.attach(7);	// 0-90-180
+  SRVup.write(0);		// to start 0
+  SRVdown.write(91);	// to start 90
+}
+
+void upchange(uint16_t DT){
+	for(uint16_t i = 0; i < 89; i++)
+	{
+		SRVup.write(i);
+		delay(DT);
+	}
+	for(uint16_t i = 89; i > 0; i--)
+	{
+		SRVup.write(i);
+		delay(DT);
+	}
+}
+
+void downchange(uint16_t DT){
+	for(uint16_t j = 91; j > 0; j--)
+	{
+		SRVdown.write(j);
+		delay(DT);
+	}
+	for (uint16_t j = 0; j < 180; j++)
+	{
+		SRVdown.write(j);
+		delay(DT);
+	}
+	for (uint16_t j = 180; j > 91; j--)
+	{
+		SRVdown.write(j);
+		delay(DT);
+	}
+}
+
+void loop() 
+{ 
+	upchange(20);
+	delay(1000);
+	downchange(20);
+	delay(1000);
+
+	downchange(28);
+	delay(1500);
+	upchange(28);
+	delay(1000);
+
+	downchange(20);
+  delay(1000); 
+	upchange(15);
+	delay(100); 
+  upchange(10);
+  
+	delay(5000);
+}
