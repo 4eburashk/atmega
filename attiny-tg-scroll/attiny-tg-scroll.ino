7025ce295265ed6daf55949fb2cae3b6426c1ad2
4eburashk <39623246+4eburashk@users.noreply.github.com>
2020-04-12 06:25:27 +0300

    attiny85 digispark hid mouse auto-scroll up/down

--- attiny-tg-scroll/attiny-tg-scroll.ino
+++ attiny-tg-scroll/attiny-tg-scroll.ino
@@ -1,38 +1,36 @@
-// attiny85 digispark mouse scroll
+// attiny85 digispark hid mouse scroll up/down
 // 4eburashk https://csn.net4me.net
-// Прикиываемся мышкой. Произвольно кроллим, останавливаемся на чтение.
+// Прикидываемся мышкой. Произвольно скроллим, останавливаемся на чтение.
 // По клику кнопки в прерывании меняем направление скролла (up/down)
+// можно и без кнопки - по умолчанию скроллит вверх.
 
 #include <DigiMouse.h>
 volatile bool FLAG = 0; // state up/down.
 volatile uint32_t milsLast;
 long rnd;     // random
-int bigwait;  // wait read
+int bigwait;  // read
 
-void setup() {
+void setup(){
   DigiMouse.begin();
   pinMode(2, INPUT_PULLUP); // P2 - button - gnd.
   randomSeed(analogRead(0));// shuffle перетряхиваем псевдо-рандом.
   attachInterrupt(0, btnInterr, FALLING); // прерывание P2 INT0
 }
 
-void loop() {
-  // Иммитация пальца для смартфона.
-  rnd = random(7, 11);      // произвольный скролл 7-11
-  bigwait = 5350 + rnd * 3; // произвольная иммитация чтения 5371-5427
-  for (int i = 0; i < rnd; ++i) {
-    DigiMouse.delay(75);
-    DigiMouse.scroll((FLAG)?-1:1);
+void loop(){
+  rnd = random(7, 11);               // произвольный скролл 7-11
+  bigwait = 5350 + rnd * 3;          // произвольная иммитация чтения 5371-5427мс
+  for (int i = 0; i < rnd; ++i){
+    DigiMouse.delay(75);             // скорость прокрутки
+    DigiMouse.scroll((FLAG)?-1:1);   // процесс скролла.
   }
-  // зависли подольше. Типа читаем.
-  DigiMouse.delay(bigwait);
+  DigiMouse.delay(bigwait);          // зависли подольше. Типа читаем.
 }
 
 void btnInterr() {
   if (millis() - milsLast > 300){
-    // первый раз тут. поэтому:
-    milsLast = millis(); // засекли и пока не время - игнорим всё остальное.
-    FLAG = !FLAG;        // передёрнули флаг.
-  } // Остальные обращения к прерыванию, считаем лишним дребезгом в течении 300мс.
+    milsLast = millis();             // засекли время - игнорим всё остальное.
+    FLAG = !FLAG;
+  } // Остальные обращения к прерыванию - считаем лишним дребезгом в течении 300мс.
 }
 // END
